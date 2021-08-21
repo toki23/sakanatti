4931f391f999dd1cf8f9830816a05130d7fef456
toki23 <blancshio@gmail.com>
2021-08-21 13:02:18 +0900

    [追加と変更]fishActivityを追加し、MainActivityに偏移するボタンを追加

---
+++ app/src/main/java/com/example/sakanatti/FishActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sakanatti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fish)
+    }
+}
--- app/src/main/java/com/example/sakanatti/MainActivity.kt
+++ app/src/main/java/com/example/sakanatti/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sakanatti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val intent = Intent(this, FishActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

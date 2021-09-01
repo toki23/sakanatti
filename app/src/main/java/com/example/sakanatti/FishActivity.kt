27899196389ffb0d0eb1895320ad795c26eaf14f
haruya0411 <m21039@g.metro-cit.ac.jp>
2021-09-01 18:11:34 +0900

    aaa

--- app/src/main/java/com/example/sakanatti/FishActivity.kt
+++ app/src/main/java/com/example/sakanatti/FishActivity.kt
@@ -14,6 +14,7 @@ import android.os.Parcelable
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity

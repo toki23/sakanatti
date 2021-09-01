5277de50aac7ab1c31a2627496349e405df64907
seimayuu <seimayuu@gmail.com>
2021-09-02 08:56:37 +0900

    when文の結合

--- app/src/main/java/com/example/sakanatti/FishActivity.kt
+++ app/src/main/java/com/example/sakanatti/FishActivity.kt
@@ -1,37 +1,37 @@
 package com.example.sakanatti
-
-import android.app.ActionBar
-import android.content.Context
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
-import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.random.Random
 
 
 class FishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fish)
-        var count: Int = 0
+        var count = 0
         val btn1: Button = findViewById(R.id.id)
         val tx1: TextView = findViewById(R.id.textView)
         val data = Database.select(applicationContext)
-        var score0 = data[0]
-        var score1 = data[1]
-        var score2 = data[2]
-        var score3 = data[3]
+        val img2 : ImageView = findViewById(R.id.img2)
+        img2.setImageResource(R.drawable.beer_1)
+        val img3 :ImageView= findViewById(R.id.img3)
+        img3.setImageResource(R.drawable.gomi_3)
+        val img4 :ImageView= findViewById(R.id.img4)
+        img4.setImageResource(R.drawable.banana_1)
+        val img5 :ImageView= findViewById(R.id.img5)
+        img5.setImageResource(R.drawable.sinnbunn_1)
+        img2.visibility = View.INVISIBLE
+        img3.visibility = View.INVISIBLE
+        img4.visibility = View.INVISIBLE
+        img5.visibility = View.INVISIBLE
+        val score0 = data[0]
+        val score1 = data[1]
+        val score2 = data[2]
+        val score3 = data[3]
+
         btn1.setOnClickListener {
             count++
 
@@ -40,9 +40,11 @@ class FishActivity : AppCompatActivity() {
              1 -> when (score0) {
                1 -> {
                    tx1.text = "床がかなり散らかっています。"
+                   img2.visibility = View.VISIBLE
                }
                2 -> {
                    tx1.text = "床が散らかっています。"
+                   img2.visibility = View.VISIBLE
                }
                3 -> {
                    tx1.text = "床を掃除しよう。"
@@ -57,9 +59,11 @@ class FishActivity : AppCompatActivity() {
              2 -> when (score1) {//1
                1 -> {
                    tx1.text = "机の上がかなり散らかっています。"
+                   img3.visibility = View.VISIBLE
                }
                2 -> {
                    tx1.text = "机が散らかっています。"
+                   img3.visibility = View.VISIBLE
                }
                3 -> {
                    tx1.text = "机を掃除しよう。"
@@ -74,9 +78,11 @@ class FishActivity : AppCompatActivity() {
             3  -> when (score2) {//2
                1 -> {
                    tx1.text = "本がかなり散らかっています。"
+                   img4.visibility = View.VISIBLE
                }
                2 -> {
                    tx1.text = "本が散らかっています。"
+                   img4.visibility = View.VISIBLE
                }
                3 -> {
                    tx1.text = "本を掃除しよう。"
@@ -91,9 +97,11 @@ class FishActivity : AppCompatActivity() {
            4 -> when (score3) {//3
                1 -> {
                    tx1.text = "服がかなり散らかっています。"
+                   img5.visibility = View.VISIBLE
                }
                2 -> {
                    tx1.text = "服が散らかっています。"
+                   img5.visibility = View.VISIBLE
                }
                3 -> {
                    tx1.text = "服を掃除しよう。"
@@ -104,8 +112,11 @@ class FishActivity : AppCompatActivity() {
                5 -> {
                    tx1.text = "服はきれいですね。"
                }
-
-
+           }
+                5 -> {
+                    tx1.text = "タップしてゲームを開始"
+                    //val intent = Intent(this,SubActivity0::class.java)
+                    //startActivity(intent)
                }
             }
         }

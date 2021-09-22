99637460348c1a258edb24c35f7aecfcd504a743
seimayuu <seimayuu@gmail.com>
2021-09-22 17:31:43 +0900

    すべてその他の場合の処理の追加と、ゲーム遷移時の表示が出るよう修正

--- app/src/main/java/com/example/sakanatti/FishActivity.kt
+++ app/src/main/java/com/example/sakanatti/FishActivity.kt
@@ -35,8 +35,15 @@ class FishActivity : AppCompatActivity() {
 
         btn1.setOnClickListener {
             count++
+            if (score0 == 6 && score1 == 6 && score2 == 6 && score3 == 6 && count ==1){
+                tx1.text = "タップしてゲームを開始"
+            }else if (score0 == 6 && score1 == 6 && score2 == 6 && score3 == 6 && count ==2){
+                val intent = Intent(this,GameView::class.java)
+                startActivity(intent)
+            }
 
-            //textView.text = "${count}"
+
+
             when (count) {
              1 -> when (score0) {
                1 -> {
@@ -56,6 +63,7 @@ class FishActivity : AppCompatActivity() {
                5 -> {
                    tx1.text = "床はきれいですね。"
                }
+
                 }
              2 -> when (score1) {//1
                1 -> {
@@ -114,10 +122,11 @@ class FishActivity : AppCompatActivity() {
                    tx1.text = "服はきれいですね。"
                }
            }
-                5 -> {
-                    tx1.text = "タップしてゲームを開始"
-                    val intent = Intent(this,GameView::class.java)
-                    startActivity(intent)
+           5,6-> {if (count ==5){
+               tx1.text = "タップしてゲームを開始"
+           }else{val intent = Intent(this,GameView::class.java)
+               startActivity(intent)   }
+
                }
             }
         }

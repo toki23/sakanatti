74c16ce47a87e3d0d2ce71e977dc14318f1e9ee5
seimayuu <seimayuu@gmail.com>
2021-09-23 15:45:02 +0900

    点数が画面にでるように表示

--- app/src/main/java/com/example/sakanatti/FishGame.kt
+++ app/src/main/java/com/example/sakanatti/FishGame.kt
@@ -2,10 +2,21 @@ package com.example.sakanatti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class FishGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fish_game)
+
+        val score5: TextView = findViewById(R.id.score_game)
+        var num = 185
+        val score = num.toString(10)
+        var text = "score:$score"
+        score5.text = text
+
+
     }
+
 }

e0b1b64be1e32234d54a20ad6f1eed68df6c3894
tknk0369 <tknk0369@users.noreply.github.com>
2021-09-24 22:53:28 +0900

    動作改善

--- app/src/main/java/com/example/sakanatti/FishView.kt
+++ app/src/main/java/com/example/sakanatti/FishView.kt
@@ -1,4 +1,38 @@
 package com.example.sakanatti
 
-class FishView {
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.sin
+
+class FishView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private var i = 0
+    private val paint: Paint = Paint()
+    private val bmp = BitmapFactory.decodeResource(resources, R.drawable.fish2_blue)
+    private val bmp0 = BitmapFactory.decodeResource(resources, R.drawable.e0760)
+    private val dst = Rect()
+    private val dst0 = Rect()
+    override fun onDraw(canvas: Canvas) {
+        val fishL = width / 3 + (150 * sin(0.1 * i.toDouble())).toInt()
+        val fishT = height / 2 + (50 * sin(0.05 * i.toDouble())).toInt()
+        val fishR = width / 3 + 500 + (150 * sin(0.1 * i.toDouble())).toInt()
+        val fishB = height / 2 + 400 + (50 * sin(0.05 * i.toDouble())).toInt()
+        dst.set(fishL, fishT, fishR, fishB)
+        dst0.set(
+            width / 3 - 200, height / 2 - 500,
+            width / 3 + 300, height / 2 - 100
+        )
+        i++
+        canvas.drawBitmap(bmp, null, dst, paint)
+        canvas.drawBitmap(bmp0, null, dst0, paint)
+        invalidate()
+    }
 }

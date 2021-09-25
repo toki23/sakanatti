6e57349ceab28d70c4834e57f39d71bd93bc7486
鷺空汰 <blancshio@gmail.com>
2021-09-25 17:57:31 +0900

    FishViewもアクティビティに移動、ただレイアウト崩れた

--- app/src/main/java/com/example/sakanatti/FishActivity.kt
+++ app/src/main/java/com/example/sakanatti/FishActivity.kt
@@ -1,13 +1,22 @@
 package com.example.sakanatti
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Math.sin
 
 
 class FishActivity : AppCompatActivity() {
@@ -15,6 +24,10 @@ class FishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_fish)
+        val wrapContent = ViewGroup.LayoutParams.WRAP_CONTENT
+        val linearLayout: LinearLayout = findViewById(R.id.fishLayout)
+        val myView = FishView(this)
+        linearLayout.addView(myView, LinearLayout.LayoutParams(wrapContent,wrapContent))
         var count = 0
         val btn1: Button = findViewById(R.id.id)
         val tx1: TextView = findViewById(R.id.textView)
@@ -133,6 +146,33 @@ class FishActivity : AppCompatActivity() {
 
                 }
             }
+        }
+    }
+    internal inner class FishView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+    ) : View(context, attrs, defStyleAttr) {
+        private var i = 0
+        private val paint: Paint = Paint()
+        private val bmp = BitmapFactory.decodeResource(resources, R.drawable.fish2_blue)
+        private val bmp0 = BitmapFactory.decodeResource(resources, R.drawable.e0760)
+        private val dst = Rect()
+        private val dst0 = Rect()
+        override fun onDraw(canvas: Canvas) {
+            val fishL = width / 3 + (150 * sin(0.1 * i.toDouble())).toInt()
+            val fishT = height / 2 + (50 * sin(0.05 * i.toDouble())).toInt()
+            val fishR = width / 3 + 500 + (150 * sin(0.1 * i.toDouble())).toInt()
+            val fishB = height / 2 + 400 + (50 * sin(0.05 * i.toDouble())).toInt()
+            dst.set(fishL, fishT, fishR, fishB)
+            dst0.set(
+                width / 3 - 200, height / 2 - 500,
+                width / 3 + 300, height / 2 - 100
+            )
+            i++
+            canvas.drawBitmap(bmp, null, dst, paint)
+            canvas.drawBitmap(bmp0, null, dst0, paint)
+            invalidate()
         }
     }
 }
